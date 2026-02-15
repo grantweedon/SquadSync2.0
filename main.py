ba1f785dff1d9132f1a5bc8d061501e026efc199
grantweedon <grant.weedon@gmail.com>
2026-02-15 21:11:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -83,4 +83,4 @@ def update_availability():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
